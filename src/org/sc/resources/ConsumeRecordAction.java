d055403175484ca94a3e70e180ec96c50b4fc48d
lazet <lizhantao@gmail.com>
2010-12-20 19:42:59 +0800

    add regret module

--- src/org/sc/resources/ConsumeRecordAction.java
+++ src/org/sc/resources/ConsumeRecordAction.java
@@ -16,12 +16,14 @@ import org.sc.security.MongoDbUtil;
 import org.sc.util.DateUtil;
 import org.sc.util.GeneralResult;
 import org.sc.util.IdUtil;
+import org.sc.util.StringUtil;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.util.JSON;
@@ -44,6 +46,26 @@ public class ConsumeRecordAction {
 		dbo.put(KEYWORDS, generatedKeyWords(dbo,new HashSet<String>()));
 		dbc.save(dbo);
 		return new  GeneralResult("PURCHASED_PRODUCT_SAVED" ,dbo).toString();
+	}
+	@RequestMapping("/regret")
+	public @ResponseBody String add(@RequestParam("id") String id, @RequestParam("reason") String reason) {
+		DBCollection dbc = MongoDbUtil.getCurrentDb().getCollection(CONSUME_RECORD);
+		DBObject dbo = dbc.findOne(new BasicDBObject("id",id).append(MERCHANT_ID, MongoDbUtil.getCurrentUnitName()));
+		if(dbo == null)
+			return new GeneralResult("regret.failed",StringUtil.toIso8859("要悔单的交易不存在")).toString();
+		if(dbo.get(StringUtil.toIso8859("状态")) == StringUtil.toIso8859("已悔")){
+			return new GeneralResult("regret.failed",StringUtil.toIso8859("此单已悔，不能再悔单")).toString();
+		}
+		else{
+			dbo.put(StringUtil.toIso8859("状态"), StringUtil.toIso8859("已悔"));
+		}
+		BasicDBObject regret = new BasicDBObject(StringUtil.toIso8859("悔单原因"),reason)
+			.append(StringUtil.toIso8859("悔单时间"), DateUtil.getCurrentTime())
+			.append(StringUtil.toIso8859("悔单操作员"), MongoDbUtil.getCurrentLoginName());
+		dbo.put(StringUtil.toIso8859("悔单信息"), regret);
+		dbo.put(KEYWORDS, generatedKeyWords(dbo,new HashSet<String>()));
+		dbc.save(dbo);
+		return new  GeneralResult("PURCHASED_PRODUCT_REGRETED" ,dbo).toString();
 	}
 	protected Set<String> generatedKeyWords(DBObject dbo,Set<String> unindexItemNames){
 		Set<String> keySet = dbo.keySet();
